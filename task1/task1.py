c59b330673fbed90d5295d459e906d8d30af6b38
MIneBoyMB <ab.ghouri03@gmail.com>
2020-05-10 16:18:17 +0500

    latest updates

--- task1/task1.py
+++ task1/task1.py
@@ -1,5 +1,5 @@
 import datetime
-from textwrap import dedent
+
 
 class Arrival(object):
     def __init__(self, cnic, phone, time, token, departure_time):
@@ -9,13 +9,7 @@ class Arrival(object):
         self.token = token
         self.departure_time = departure_time
     def getter(self):
-        return (dedent(f"""
-            CNIC      : {self.cnic}
-            Phone #   : {self.phone}
-            Time      : {self.time}
-            Token     : {self.token}
-            Departure : {self.departure_time}
-                """))
+        return (f"{self.cnic}~{self.phone}~{self.time}~{self.token}~{self.departure_time} \n")
     def change_departure_time(self, new):
         self.departure_time = new
 
@@ -34,8 +28,7 @@ while True:
         phone = input("Input Phone Number")
         time  = datetime.datetime.now()
         data_arrival[token] = Arrival(cnic, phone, time, token, '0')
-        print(data_arrival)
-        print(token)
+        print(f"Your token is {token}")
         token += 1
 
         ##print((data_arrival[token]).getter())
@@ -48,6 +41,7 @@ while True:
         f = open(f"{datetime.date.today()}.txt","w")
         for i in range(token):
             f.write(data_arrival[i].getter())
-            
+
+
         f.close()
         break

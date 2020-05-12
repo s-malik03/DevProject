68c1b4de81a3a19dacc21daa5c46b223e294054d
alizaidi <alizaidi22003@gmail.com>
2020-05-12 15:33:02 +0500

    added db functionality

---
+++ web/db.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+db = sqlite3.connect('example.db')
+c = db.cursor()
+
+c.execute('''CREATE TABLE information (CNIC, PHONE, ARRIVAL_TIME, DEPARTURE_TIME)''')
+
+def insert(db):
+    sql  = "INSERT INTO information VALUES (%s, %s %s %s)"
+    val  = (db.cnic, db.phone, db.arrival_time, db.departure_time)
+    c.execute(sql, val)
